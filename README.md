da799711b7838025d235ece0e5c0598900cb38a1
Aadil <aadil.hasan07@gmail.com>
2021-07-03 18:34:27 +0530

    added test cases and updated readme

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # react-ctx-store
 
-> Made with create-react-library
+> A small react-context wrapper, which makes context more performent, scalable and easy to use, scalable and easy to use for large projects.
+
+## Why?
+
+React context is good, but it is not good for medium to big projects, it has following problems:
+
+- re-rendering, context re-renders all the subsribers doesn't metter if the subsriber is using the updated property or not.
+- scalability, context has this re-rendering issue, one single store is not possible so multiple context are required.
+- there is not simple way to update the context, it requires a bit of extra code.
 
 [![NPM](https://img.shields.io/npm/v/react-ctx-store.svg)](https://www.npmjs.com/package/react-ctx-store) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
 
@@ -10,20 +18,119 @@
 npm install --save react-ctx-store
 ```
 
-## Usage
+### Usage
 
 ```tsx
 import React, { Component } from 'react'
 
-import MyComponent from 'react-ctx-store'
-import 'react-ctx-store/dist/index.css'
+import { createContext } from 'react-ctx-store'
+
+const store = {
+  count: 0,
+}
+
+const updaters = {
+  updateCount: (store) => {
+    return {
+      count: store.count + 1;
+    }
+  }
+}
+
+const {Consumer, Provider} = createContext(store, updaters)
+
+const CounterComponent = ({count, updateCount}) => {
+  return (
+    <div>
+      <span>{count}</span>
+      <button onClick={updateCount}>update count</button>
+    </div>
+  )
+}
 
 class Example extends Component {
   render() {
-    return <MyComponent />
+    return (
+      <Provider>
+        <Consumer mapContextToProps={['count', 'updateCount]}>
+          <CounterComponent/>
+        </Consumer>
+      </Provider>
+    )
   }
 }
 ```
+
+### API
+
+```tsx
+import { creatContext } from 'react-ctx-store'
+
+const { Consumer, Provider, connect, _context } = createContext(store, updaters)
+```
+
+`react-ctx-store` exposes one single method `createContext`, which returns following Components and methods:
+
+#### 1. Provider
+
+A React component allows consuming components to subscribe to context changes, similar to React Context Provider.
+
+Usage:
+
+```tsx
+function Example() {
+  return (
+    /* children of the Provider will have access to store and updaters using Provider Component */
+    <Provider>
+      <App />
+    </Provider>
+  )
+}
+```
+
+#### 2. Consumer
+
+A React Component that subscribe to the values of the store/context.
+Consumer takes a single prop `mapContextToProps` which take an array of properties or a callback function as value
+
+Usage:
+
+```tsx
+/* all direct children of Consumer component will receive count as a prop */
+<Consumer mapContextToProps={['count']}>...</Consumer>
+```
+
+or
+
+```tsx
+/* all direct children of Consumer component will receive totalCount as a prop */
+<Consumer mapContextToProps={({count} => {totalCount: count * 10})}>
+  ...
+</Consumer>
+```
+
+#### 3. Connect
+
+A method which lets you subscribe to the values of the store/context.
+
+Usage:
+
+```tsx
+/* Component will receive count as a prop*/
+const App = connect(['count'])(Component)
+```
+
+or
+
+```tsx
+/* Component will receive count as a prop*/
+const App = connect((store) => {count: store.count, update: store.updateCount})(Component)
+```
+
+#### 4. \_context
+
+A React Context object which is being used to store the the values, it is being exposted in cause a user wants to have acess to original Context.
+A good use case example would be using \_context with `useContext` hooks.
 
 ## License
 
--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,7 +1,13 @@
-import { createContext } from '.'
 import React from 'react'
+import { createContext } from '.'
+
+import Enzyme from 'enzyme'
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
+
+Enzyme.configure({ adapter: new Adapter() })
 const State = {
-  count: 0
+  count: 0,
+  time: Date.now()
 }
 
 const updater = {
@@ -9,35 +15,131 @@ const updater = {
     return {
       count: state.count + 1
     }
+  },
+  updateTime: () => {
+    return {
+      time: Date.now()
+    }
   }
 }
 
 const { Provider, Consumer } = createContext(State, updater)
 
-class Comp extends React.Component<Partial<typeof State & typeof updater>> {
+interface CompProps extends Partial<typeof State & typeof updater> {
+  didUpdate: () => {}
+}
+class Comp extends React.Component<CompProps> {
+  componentDidUpdate({ didUpdate }) {
+    if (didUpdate && this.props.didUpdate) {
+      this.props.didUpdate()
+    }
+  }
   render() {
     const { count, updateCount } = this.props
     return (
       <div>
         <span id='count'>{count}</span>
-        <button onClick={updateCount}>Update Count</button>
+        <button id='update' onClick={updateCount}>
+          Update Count
+        </button>
       </div>
     )
   }
 }
 
-const App = () => {
+class Comp2 extends React.Component<CompProps> {
+  componentDidUpdate({ didUpdate }) {
+    if (didUpdate && this.props.didUpdate) {
+      this.props.didUpdate()
+    }
+  }
+  render() {
+    const { time, updateTime } = this.props
+    return (
+      <div>
+        <span id='time'>{time}</span>
+        <button id='update-time' onClick={updateTime}>
+          Update Time
+        </button>
+      </div>
+    )
+  }
+}
+
+const App = ({ didUpdate1, didUpdate2 }: any) => {
   return (
     <Provider>
       <Consumer mapContextToProps={['count', 'updateCount']}>
-        <Comp />
+        <Comp didUpdate={didUpdate1} />
+      </Consumer>
+      <Consumer mapContextToProps={['time', 'updateTime']}>
+        <Comp2 didUpdate={didUpdate2} />
       </Consumer>
     </Provider>
   )
 }
 
 describe('ExampleComponent', () => {
-  it('is truthy', () => {
-    expect(ExampleComponent).toBeTruthy()
+  let app
+  beforeEach(() => {
+    app = Enzyme.mount(<App />)
+  })
+
+  it('works as expect', () => {
+    const instance1 = app.find(Comp).at(0).instance()
+    expect(instance1.props).toHaveProperty('count', State.count)
+    expect(instance1.props).toHaveProperty('updateCount')
+    expect(app.find('#count').html()).toMatch('' + State.count)
+
+    const instance2 = app.find(Comp2).at(0).instance()
+    expect(instance2.props).toHaveProperty('time', State.time)
+    expect(instance2.props).toHaveProperty('updateTime')
+    expect(app.find('#time').html()).toMatch('' + State.time)
+  })
+
+  it('should update', async () => {
+    let fn = jest.fn()
+    app.setProps({ didUpdate1: fn })
+    app.find('#update').simulate('click')
+    let instance = app.find(Comp).at(0).instance()
+    expect(fn).toHaveBeenCalledTimes(1)
+    expect(instance.props).toHaveProperty('count', 1)
+    expect(app.find('#count').html()).toMatch('' + (State.count + 1))
+  })
+
+  it('should update2', async () => {
+    let fn = jest.fn()
+    app.setProps({ didUpdate2: fn })
+    app.find('#update-time').simulate('click')
+    expect(fn).toHaveBeenCalledTimes(1)
+    expect(app.find('#time').html()).toBeTruthy()
+    expect(app.find('#time').html()).not.toMatch('' + State.time)
+  })
+
+  it('Comp2 should not re-render if Comp1 subscribed context value changed and Comp2 has subscribed to differnet context values then Comp1', () => {
+    let fn = jest.fn()
+    let fn2 = jest.fn()
+    app.setProps({
+      didUpdate1: fn,
+      didUpdate2: fn2
+    })
+    app.find('#update').simulate('click')
+    expect(fn).toBeCalledTimes(1)
+    expect(fn2).not.toBeCalled()
+  })
+
+  it('Comp1 should not re-render if Comp2 subscribed context value changed and Comp1 has subscribed to differnet context values then Comp2', () => {
+    let fn = jest.fn()
+    let fn2 = jest.fn()
+    app.setProps({
+      didUpdate1: () => {
+        console.log('called')
+        fn()
+      },
+      didUpdate2: fn2
+    })
+    app.find('#update-time').simulate('click')
+    expect(fn2).toBeCalledTimes(1)
+    expect(fn).not.toBeCalled()
   })
 })
